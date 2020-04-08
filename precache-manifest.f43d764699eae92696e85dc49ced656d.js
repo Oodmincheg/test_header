56fe3c125f5e6e8d53873fadb93f0f6110e3b1f1
Vorobiov Vlad <vorobyov.vladislav@gmail.com>
2020-04-08 16:04:39 +0300

    Updates

---
+++ precache-manifest.f43d764699eae92696e85dc49ced656d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b37bfaa9d6537a059cb8c23d09fcd11",
+    "url": "/test_header/index.html"
+  },
+  {
+    "revision": "1a0a785f921aa4517aec",
+    "url": "/test_header/static/css/main.5ac0ca8c.chunk.css"
+  },
+  {
+    "revision": "94dca94dc9b915a8c973",
+    "url": "/test_header/static/js/2.62741b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_header/static/js/2.62741b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a0a785f921aa4517aec",
+    "url": "/test_header/static/js/main.4c41906a.chunk.js"
+  },
+  {
+    "revision": "6d129ab236dbc6056425",
+    "url": "/test_header/static/js/runtime-main.861e4647.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test_header/static/media/logo.5d5d9eef.svg"
+  }
+]);
